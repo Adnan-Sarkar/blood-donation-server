73e548df80f47a2f84d96fb853bb98b11332f6fc
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-04-05 15:49:35 +0600

    Add env variables into config

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,4 +7,6 @@ dotenv.config({
 
 export const config = {
   PORT: process.env.PORT,
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN,
 };
