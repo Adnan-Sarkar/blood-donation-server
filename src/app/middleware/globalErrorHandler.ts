b649a325c579378b291c829c48f2813c13197719
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-04-06 10:39:28 +0600

    Update globalErrorHandler with zod error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,12 +1,22 @@
 import { ErrorRequestHandler } from "express";
 import httpStatus from "http-status";
+import { ZodError } from "zod";
+import zodErrorHandler from "../error/zodErrorHandler";
 
 const globalErrorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
-  res.status(httpStatus.BAD_REQUEST).json({
+  const errorResponse = {
     success: false,
     message: error.message || "error mesage",
     errorDetails: error,
-  });
+  };
+
+  if (error instanceof ZodError) {
+    const { message, issues } = zodErrorHandler(error);
+    errorResponse.message = message;
+    errorResponse.errorDetails = { issues };
+  }
+
+  res.status(httpStatus.BAD_REQUEST).json(errorResponse);
 };
 
 export default globalErrorHandler;
