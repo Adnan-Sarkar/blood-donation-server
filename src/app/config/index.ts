533a837f607bd4e0a5342de9801c5e1686e7126b
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-04-06 11:45:38 +0600

    Add env variable into config

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -9,4 +9,5 @@ export const config = {
   PORT: process.env.PORT,
   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
   JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN,
+  SALT_ROUNDS: process.env.SALT_ROUNDS,
 };
