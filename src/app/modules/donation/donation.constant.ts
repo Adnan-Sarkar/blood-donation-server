78daeadd5fb099c96fcd7acf09ffc0cb2651e1ab
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-04-08 02:33:13 +0600

    Create donor list sort fields name

--- src/app/modules/donation/donation.constant.ts
+++ src/app/modules/donation/donation.constant.ts
@@ -1,3 +1,5 @@
 export const donorListQueryParams = ["searchTerm", "bloodType", "availability"];
 
 export const donorListSearchFields = ["bloodType", "location", "name", "email"];
+
+export const donorListSortByFields = ["lastDonationDate", "age", "name"];
