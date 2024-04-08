b54a676bffc981cd9c74e545013470c112e3295f
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-04-08 15:49:21 +0600

    Update search fields name for donor list

--- src/app/modules/donation/donation.constant.ts
+++ src/app/modules/donation/donation.constant.ts
@@ -1,5 +1,5 @@
 export const donorListQueryParams = ["searchTerm", "bloodType", "availability"];
 
-export const donorListSearchFields = ["bloodType", "location", "name", "email"];
+export const donorListSearchFields = ["location", "name", "email"];
 
 export const donorListSortByFields = ["lastDonationDate", "age", "name"];
