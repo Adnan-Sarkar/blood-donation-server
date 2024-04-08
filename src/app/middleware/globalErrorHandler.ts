5ffef495f286ab7f5ceee6bab51a95d408f004c7
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-04-08 17:16:58 +0600

    Update globalErrorHandler middlewre with proper status code

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -2,6 +2,7 @@ import { ErrorRequestHandler } from "express";
 import httpStatus from "http-status";
 import { ZodError } from "zod";
 import zodErrorHandler from "../error/zodErrorHandler";
+import AppError from "../error/AppError";
 
 const globalErrorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
   const errorResponse = {
@@ -10,13 +11,19 @@ const globalErrorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
     errorDetails: error,
   };
 
+  let statustCode: number = httpStatus.INTERNAL_SERVER_ERROR;
+
   if (error instanceof ZodError) {
     const { message, issues } = zodErrorHandler(error);
     errorResponse.message = message;
     errorResponse.errorDetails = { issues };
+    statustCode = httpStatus.BAD_REQUEST;
+  } else if (error instanceof AppError) {
+    errorResponse.message = error.message;
+    statustCode = error.statusCode;
   }
 
-  res.status(httpStatus.BAD_REQUEST).json(errorResponse);
+  res.status(statustCode).json(errorResponse);
 };
 
 export default globalErrorHandler;
