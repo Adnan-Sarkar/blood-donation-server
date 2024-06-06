f185060bc25989e56ce838c9306be0df5d0ddc77
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-07 00:28:06 +0600

    Update auth types

--- src/app/modules/auth/auth.types.ts
+++ src/app/modules/auth/auth.types.ts
@@ -1,14 +1,17 @@
-import { BloodType } from "@prisma/client";
+import { BloodType, Gender, UserRole } from "@prisma/client";
+
 
 export type TRegistration = {
   name: string;
   email: string;
   password: string;
   bloodType: BloodType;
+  gender: Gender;
   location: string;
   age: number;
   bio: string;
   lastDonationDate: string;
+  role: keyof typeof UserRole;
 };
 
 export type TLogin = {
