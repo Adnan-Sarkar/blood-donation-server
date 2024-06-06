f4e7a316cf0f2ad87db473a9d327ddb4d05387a1
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-07 00:29:45 +0600

    Update config for env variables

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -8,6 +8,8 @@ dotenv.config({
 export const config = {
   PORT: process.env.PORT,
   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
   JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN,
+  JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN,
   SALT_ROUNDS: process.env.SALT_ROUNDS,
 };
