7db656a7afb4f1cd6e9e039aa1b381e4040c22a8
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-07 00:30:47 +0600

    Update jwt payload type

--- src/app/types/jwtPayload.type.ts
+++ src/app/types/jwtPayload.type.ts
@@ -1,7 +1,10 @@
+import { UserRole } from '@prisma/client';
+
 type TJWTPayload = {
   id: string;
   name: string;
   email: string;
+  role: keyof typeof UserRole;
 };
 
 export default TJWTPayload;
