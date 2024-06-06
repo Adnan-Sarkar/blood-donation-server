4c861abc3e3c2ba1071fd5364caf41d8e33a3d39
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-07 00:46:41 +0600

    Update auth middleware

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -3,28 +3,39 @@ import AppError from "../error/AppError";
 import catchAsync from "../utils/catchAsync";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { config } from "../config";
+import { UserRole } from "../constant/userRole";
 
-const auth = () => {
+const auth = (...userRoles: (keyof typeof UserRole)[]) => {
   return catchAsync((req, _res, next) => {
     const token = req.headers.authorization as string;
 
-    let decode;
-
-    try {
-      decode = jwt.verify(
-        token,
-        config.JWT_ACCESS_SECRET as string
-      ) as JwtPayload;
-    } catch (error: any) {
+    if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
     }
 
-    req.user = {
-      id: decode.id,
-      name: decode.name,
-      email: decode.email,
-    };
-    next();
+    let verifiedUser;
+
+    try {
+      verifiedUser = jwt.verify(
+        token,
+        config.JWT_ACCESS_SECRET as string
+      ) as JwtPayload;
+
+      req.user = {
+        id: verifiedUser.id,
+        name: verifiedUser.name,
+        email: verifiedUser.email,
+        role: verifiedUser.role,
+      };
+
+      if (userRoles.length > 0 && !userRoles.includes(verifiedUser.role)) {
+        throw new AppError(httpStatus.FORBIDDEN, "You are forbidden!");
+      } else {
+        next();
+      }
+    } catch (error: any) {
+      next(error);
+    }
   });
 };
 
