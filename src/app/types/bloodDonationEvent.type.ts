e11ed3fa3f0c76c20467654b1ed3838f3ec1d85c
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-26 19:18:03 +0600

    Create blood donation event type

---
+++ src/app/types/bloodDonationEvent.type.ts
@@ -0,0 +1,9 @@
+export type TBloodDonationEventType = {
+    eventTitle: string;
+    eventLocation: string;
+    eventDate: string;
+    eventTime: string;
+    contactNumber: string;
+    eventCoverPhoto?: string;
+    isComplete?: boolean;
+}
